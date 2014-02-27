01f153c798748f26bd85f9c40a11e0279eb032b0
Alexander Fosseidbraaten <fosseidb@stud.ntnu.no>
2014-02-27 10:58:19 -0800

    analytics lab

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,14 @@ var projects = require('../projects.json');
  */
 
 exports.view = function(req, res){
+
+	projects.grid = false;
+	console.log("noGrid" + projects[1]);
   	res.render('index', projects);
 };
+
+exports.viewGrid = function(req, res){
+	projects.grid = true;
+	console.log("grid" + projects[1]);
+	res.render('index', projects);
+}
